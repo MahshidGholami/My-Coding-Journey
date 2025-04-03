2d31707fe6fb38d6d851f65c3fd53cf242b0c0c4
mahshid <gholamimahshid22@gmail.com>
2025-04-03 09:04:32 -0700

    new file

--- concurrency/Multithreading.py
+++ concurrency/Multithreading.py
@@ -1,18 +1,48 @@
+import os
+import time
+import threading
 import requests
-from bs4 import BeautifulSoup
+
+IMAGE_URLS = [
+    "https://picsum.photos/400/300",
+    "https://picsum.photos/500/400",
+    "https://picsum.photos/600/500",
+    "https://picsum.photos/700/600",
+    "https://picsum.photos/800/700",
+]
 
 
-URL="https://www.google.com/"
-
-response=requests.get(URL)
+OUTPUT_FOLDER = "downloaded_images"
 
 
-if response.status_code == 200 :
-    soup=BeautifulSoup(response.text, "html.parser")
+if not os.path.exists(OUTPUT_FOLDER):
+    os.makedirs(OUTPUT_FOLDER)
 
+def download_image(url, index):
+  
+    try:
+        response = requests.get(url)  
+        if response.status_code == 200:
+            filename = os.path.join(OUTPUT_FOLDER, f"image_{index}.jpg")
+            with open(filename, "wb") as file:
+                file.write(response.content)  
+            print(f" تصویر {index} دانلود شد: {filename}")
+        else:
+            print(f" خطا در دانلود تصویر {index}")
+    except Exception as e:
+        print(f" خطای غیرمنتظره در تصویر {index}: {e}")
 
-    product_price=soup.find("span", class_='price').text.strip()
+if __name__ == "__main__":
+    start_time = time.time()
+    
+    threads = []  
+    for i, url in enumerate(IMAGE_URLS):
+        thread = threading.Thread(target=download_image, args=(url, i+1))
+        threads.append(thread)
+        thread.start()  #
 
-    print(f'price= {product_price}')
-else:
-    print('خطا')
+    for thread in threads:
+        thread.join()
+
+    end_time = time.time()
+    print(f" دانلود {len(IMAGE_URLS)} تصویر در {end_time - start_time:.2f} ثانیه انجام شد.")
